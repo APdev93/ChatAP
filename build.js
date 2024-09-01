dbe48b6f777fd6d924e40951f94608df55e494db
APdev93 <lokpedangeran@gmail.com>
2024-09-01 23:56:13 +0800

    fix script build up

--- build.js
+++ build.js
@@ -1,23 +1,14 @@
 const { exec } = require("child_process");
 
-exec(
-	`terser ./public/assets/app.js \
-  -c drop_console=true,drop_debugger=true \
-  -m \
-  --mangle-props regex="/^[^_]/" \
-  --toplevel \
-  --keep_fnames=false \
-  --keep_classnames=false \
-  --output ./public/assets/app.min.js`,
-	(error, stdout, stderr) => {
-		if (error) {
-			console.error(`Error: ${error.message}`);
-			return;
-		}
-		if (stderr) {
-			console.error(`Stderr: ${stderr}`);
-			return;
-		}
-		console.log(`Successfully build: ${stdout}`);
-	},
-);
+let cmd = `terser ./public/assets/app.js -c -m -o ./public/assets/app.min.js`;
+exec(cmd, (error, stdout, stderr) => {
+	if (error) {
+		console.error(`Error: ${error.message}`);
+		return;
+	}
+	if (stderr) {
+		console.error(`Stderr: ${stderr}`);
+		return;
+	}
+	console.log(`Successfully build: ${stdout}`);
+});
